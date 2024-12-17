9bc03c3fa3f64f27097a05e114c9d230dc71ad9a
tsec991-wa <112241946+tsec991-wa@users.noreply.github.com>
2024-12-18 05:58:00 +0800

    Delete app directory

--- app/helpers/url.py
+++
@@ -1,9 +0,0 @@
-from app import utils
-
-
-def get_url_by_task_id(task_id):
-    query = {
-        "task_id": task_id
-    }
-    items = utils.conn_db('url').distinct("url", query)
-    return list(items)
